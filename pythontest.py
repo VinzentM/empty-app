fa5b2f5fd3e13ae1559e2e48777e971ac0d70bd3
VinzentM <vinzentmoesch@hanovernorwichschools.org>
2016-09-02 13:28:36 -0400

    Updated from Brython Server: 9/2/2016 1:28:36 PM

--- pythontest.py
+++ pythontest.py
@@ -1,3 +1,13 @@
-from ggame import App
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+oragne = Color(0xFFD740, 1.0)
+greywhite = Color(0xF5F5F5, 1.0)
+
+thinline = LineStyle(1, greywhite)
+
+rectangle = RectangleAsset(50, 50, thinline, oragne)
+
+
 myapp = App()
 myapp.run()
